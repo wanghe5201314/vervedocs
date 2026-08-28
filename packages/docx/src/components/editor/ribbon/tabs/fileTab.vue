319b716f68599db28976ceb10b6c670de790852c
Avan0203 <1208097313@qq.com>
2026-08-28 16:52:56 +0800

    fix(docx):修复导出命名，与ppt，excel统一

--- packages/docx/src/components/editor/ribbon/tabs/fileTab.vue
+++ packages/docx/src/components/editor/ribbon/tabs/fileTab.vue
@@ -1,21 +1,81 @@
 <template>
   <div class="ribbon-tab-panel file-tab">
     <RibbonGroup title="文档操作">
-      <RibbonButton icon="file-plus-outline" text="新建文档" title="新建文档" size="large" command="new" />
-      <RibbonButton icon="file-import-outline" text="导入文档" title="导入文档 (Ctrl+Alt+O)" size="large" command="import" />
-      <RibbonButton icon="content-save-outline" text="保存" title="保存 (Ctrl+S)" size="large" command="save" />
-      <RibbonButton icon="download-outline" text="下载" title="下载为 Word" size="large" command="export" command-args="docx" />
-      <RibbonButton icon="printer-outline" text="打印" title="打印 (Ctrl+P)" size="large" command="print" />
-      <RibbonButton icon="eye-outline" text="预览" title="预览" size="large" command="preview" />
-      <RibbonButton icon="rename-box" text="重命名" title="重命名" size="large" command="rename" />
+      <RibbonButton
+        icon="file-plus-outline"
+        text="新建文档"
+        title="新建文档"
+        size="large"
+        command="new"
+      />
+      <RibbonButton
+        icon="file-import-outline"
+        text="导入文档"
+        title="导入文档 (Ctrl+Alt+O)"
+        size="large"
+        command="import"
+      />
+      <RibbonButton
+        icon="content-save-outline"
+        text="保存"
+        title="保存 (Ctrl+S)"
+        size="large"
+        command="save"
+      />
+      <RibbonButton
+        icon="download-outline"
+        text="导出文档"
+        title="导出文档"
+        size="large"
+        command="export"
+        command-args="docx"
+      />
+      <RibbonButton
+        icon="printer-outline"
+        text="打印"
+        title="打印 (Ctrl+P)"
+        size="large"
+        command="print"
+      />
+      <RibbonButton
+        icon="eye-outline"
+        text="预览"
+        title="预览"
+        size="large"
+        command="preview"
+      />
+      <RibbonButton
+        icon="rename-box"
+        text="重命名"
+        title="重命名"
+        size="large"
+        command="rename"
+      />
     </RibbonGroup>
 
     <RibbonGroup title="文档保护">
-      <RibbonButton icon="shield-lock-outline" text="保护文档" title="保护文档" size="large" command="protectDoc" />
-      <RibbonButton icon="history" text="版本历史" title="版本历史" size="large" command="versionHistory" />
-      <RibbonButton icon="account-multiple-outline" text="访问权限" title="访问权限" size="large" command="accessPermission" />
+      <RibbonButton
+        icon="shield-lock-outline"
+        text="保护文档"
+        title="保护文档"
+        size="large"
+        command="protectDoc"
+      />
+      <RibbonButton
+        icon="history"
+        text="版本历史"
+        title="版本历史"
+        size="large"
+        command="versionHistory"
+      />
+      <RibbonButton
+        icon="account-multiple-outline"
+        text="访问权限"
+        title="访问权限"
+        size="large"
+        command="accessPermission"
+      />
     </RibbonGroup>
-
   </div>
 </template>
 
@@ -32,5 +92,4 @@ defineProps<{
 }>()
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
